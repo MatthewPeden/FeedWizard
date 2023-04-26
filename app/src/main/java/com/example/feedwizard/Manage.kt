8b94f90c5eedf2ccd897b3592b2390a298ca6189
Matthew Peden <mbpeden@hotmail.com>
2023-04-25 21:50:13 -0500

    manage and mix completed

--- app/src/main/java/com/example/feedwizard/Manage.kt
+++ app/src/main/java/com/example/feedwizard/Manage.kt
@@ -1,16 +1,24 @@
 package com.example.feedwizard
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.content.SharedPreferences
+import android.widget.Toast
 import com.example.feedwizard.databinding.ActivityManageBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class Manage : AppCompatActivity() {
     private lateinit var binding: ActivityManageBinding
 
+    data class Ingredient(val name: String, val amount: Double)
+
+    private val ingredientsList = mutableListOf<Ingredient>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_manage)
 
         binding = ActivityManageBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -18,6 +26,44 @@ class Manage : AppCompatActivity() {
         binding.startMixButton.setOnClickListener {
             val intent = Intent(this, MixFeed::class.java)
             startActivity(intent)
+        }
+
+        binding.addIngredientButton.setOnClickListener {
+            val name = binding.ingredientName.text.toString()
+            val amount = binding.ingredientAmount.text.toString().toDoubleOrNull()
+
+            if (name.isNotBlank() && amount != null) {
+                val ingredient = Ingredient(name, amount)
+                ingredientsList.add(ingredient)
+                binding.ingredientName.text.clear()
+                binding.ingredientAmount.text.clear()
+
+                Toast.makeText(this, "Inventory Item Added", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please fill in both fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.startMixButton.setOnClickListener {
+            val startWeight = binding.startWeightAmount.text.toString()
+            val batch = binding.batchName.text.toString()
+
+            if (ingredientsList.isNotEmpty() && startWeight.isNotBlank() && batch.isNotBlank()) {
+                val sharedPreferences: SharedPreferences = getSharedPreferences("com.example.app.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                val gson = Gson()
+
+                val ingredientsJson = gson.toJson(ingredientsList)
+                editor.putString("ingredientsList", ingredientsJson)
+                editor.putString("startWeightAmount", startWeight)
+                editor.putString("batchName", batch)
+                editor.apply()
+
+                val intent = Intent(this, MixFeed::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please fill in all fields and add at least one ingredient", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/feedwizard/MixFeed.kt
+++ app/src/main/java/com/example/feedwizard/MixFeed.kt
@@ -1,16 +1,25 @@
 package com.example.feedwizard
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.view.View
 import com.example.feedwizard.databinding.ActivityMixFeedBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MixFeed : AppCompatActivity() {
     private lateinit var binding: ActivityMixFeedBinding
+    data class Ingredient(val name: String, val amount: Double)
+
+    private var ingredients: List<Ingredient> = emptyList()
+    private var currentIngredientIndex: Int = 0
+    private var startWeight: Double = 0.0
+    private var targetWeight: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_manage)
 
         binding = ActivityMixFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -19,6 +28,67 @@ class MixFeed : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        ingredients = getIngredientsList()
+        startWeight = getStartWeight()
+
+        if (ingredients.isNotEmpty()) {
+            targetWeight = startWeight + ingredients[currentIngredientIndex].amount
+        } else {
+            targetWeight = startWeight
+        }
+
+        updateUI()
+
+        binding.minus.setOnClickListener {
+            if (targetWeight > 0) {
+                targetWeight -= 10.0
+            }
+            updateUI()
+        }
+
+        binding.plus.setOnClickListener {
+            targetWeight += 10.0
+            updateUI()
+        }
+
+        binding.nextIngredient.setOnClickListener {
+            if (currentIngredientIndex < ingredients.size - 1) {
+                currentIngredientIndex++
+                targetWeight += ingredients[currentIngredientIndex].amount
+                updateUI()
+            }
+        }
     }
 
+    override fun onResume() {
+        super.onResume()
+        updateUI()
+    }
+
+    private fun getIngredientsList(): List<Ingredient> {
+        val gson = Gson()
+        val sharedPreferences = getSharedPreferences("com.example.app.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
+        val ingredientsJson = sharedPreferences.getString("ingredientsList", "")
+        val type = object : TypeToken<List<Ingredient>>() {}.type
+        return gson.fromJson(ingredientsJson, type) ?: emptyList()
+    }
+
+    private fun getStartWeight(): Double {
+        val sharedPreferences = getSharedPreferences("com.example.app.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("startWeightAmount", "0")?.toDouble() ?: 0.0
+    }
+
+    private fun updateUI() {
+        binding.targetWeightValue.text = String.format("%.1f", targetWeight)
+        binding.startWeightValue.text = String.format("%.1f", startWeight)
+
+        if (currentIngredientIndex < ingredients.size) {
+            val currentIngredient = ingredients[currentIngredientIndex]
+            binding.nowLoadingText.text = "Now Loading: ${currentIngredient.name} (${currentIngredient.amount} lb)"
+        } else {
+            binding.nowLoadingText.visibility = View.GONE
+            binding.nextIngredient.visibility = View.GONE
+        }
+    }
 }
